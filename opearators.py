1025af0f243a917a669093301c7c36f645b86089
moulali39 <107177595+moulali39@users.noreply.github.com>
2022-07-08 19:26:04 +0530

    Add files via upload

---
+++ opearators.py
@@ -0,0 +1,58 @@
+# a=float(input("enter the value of a:"))
+# b=float(input("enter the value of b:"))
+# # a,b=[int(x) for x in input('enter the value of a and b: ').split()]
+# print(f"the value of {a}+{b} is {a+b}")
+# print(f"the value of {a}-{b} is {a-b}")
+# print(f"the value of {a}*{b}is {a*b}")
+# print(f"the value of {a}/{b}is {a/b}")
+# print(f"the value of {a}%{b}is {a%b}")
+
+
+# name=input("enter the name:")
+# age =int(input("enter the age:"))
+# per=float(input("enterthe value of graduation percentage:"))
+
+# print("enter the name",name,"enter the age of",age, \
+#     "enter the per in graduation",per,)
+
+
+# # """""""""""""""""  2.. floor division         """"""""""""""""""""""""""
+# c=4//2
+# print(c)
+
+# a=5//2
+# print(a)
+
+# c=5.0//2
+# print(c)
+
+# d=6.0//2
+# print(d)
+
+
+# d=6//2
+# print(d)
+
+# """"""""""        3..  assignment opearators          """"""""""""""""
+
+
+# a=5;b=7
+# a=a+b
+# # print(a)
+# a=a+a
+# print(a)
+
+# a=5;b=6
+# a=a+b
+# print(a)
+# a=a+a
+# print(a)
+
+
+b=12
+b/=2
+print(b)
+
+c=56
+c/=2
+print(c)
